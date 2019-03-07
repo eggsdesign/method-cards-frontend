b716035f8652291f207028628234dc035e741081
ballexman <alexander.vanvik@gmail.com>
2019-03-07 18:03:17 +0100

    fetch content from sanity

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
+const SANITY_PROJECT_ID = 'r1vilzq1'
+
 const cardTemplate = (props) => {
   return (`
     <div class="card">
 
       <div class=card-text>
         <h2 class="card-title">${props.title}</h2>
-        <p class="card-subtext">${props.subtext}</p>
+        <p class="card-subtext">${props.subtitle}</p>
       </div>
 
       <div class="img-container">
-        <img src="${props.imgurl}" class="card-image">
+        <img src="${props.imageUrl}" class="card-image">
       </div>
 
     </div>
   `)
 }
 
-// Our hand made data array. Could also be data from third party or whatever.
-const data = [
-  {
-    title: "Kick-off workshop",
-    subtext: "Share insights, define goals",
-    imgurl: "https://images.unsplash.com/flagged/photo-1550946107-8842ae9426db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80 667w"
-  },
-  {
-    title: "Observation",
-    subtext: "Visualize and understand the service from a user's perspective",
-    imgurl: "https://images.unsplash.com/photo-1417577354685-3ab67e9716a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80 751w"
-  },
-]
-
-// Generate a container element to collect generated cards
-let cardList = document.createElement('div')
-cardList.classList.add("cards-container")
-
-// Step through all entries in the 'data' array, generate html-elements and append to cardList container element
-data.map((dataEntry) => {
-  cardList.innerHTML += cardTemplate(dataEntry)
+// Define the sanity communication module we call 'client'
+var client = window.SanityClient({
+  projectId: SANITY_PROJECT_ID,
+  dataset: 'production',
+  useCDN: true
 })
 
-// Add card container cardList to an element in the page
-document.getElementById('cards').append(cardList)
+
+// Fetch all documents of type method. "..." means get all content in the method object. The following stuff makes sure we also fetch referenced files to get the image urls
+var query = `*[_type=="method"]{
+  ...,
+  "imageUrl": image.asset->url}`
+
+
+client
+  .fetch(query) // Talk to server: request data based on query
+  .then(renderCards) // Send received data into renderCards function
+  .catch(()=>{ console.log("Error!") }) // ...but if data fetch fails, do this
+
+
+// Function to handle card rendering
+function renderCards (cardsData) {
+  // Generate a container element to collect generated cards
+  let cardList = document.createElement('div')
+  cardList.classList.add("cards-container")
+
+  // Step through all entries in the 'data' array, generate html-elements and append to cardList container element
+  cardsData.map((dataEntry) => {
+    cardList.innerHTML += cardTemplate(dataEntry)
+  })
+
+  // Add card container cardList to an element in the page
+  document.getElementById('cards').append(cardList)
+}
